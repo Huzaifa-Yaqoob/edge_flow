062b02d6de3dcbc6f253f20388c4453c2b2e86f6
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2026-05-07 18:47:37 +0500

    initialize project

---
+++ packages/core-wasm/src/lib.rs
@@ -0,0 +1,11 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn init_panic_hook() {
+    console_error_panic_hook::set_once();
+}
+
+#[wasm_bindgen]
+pub fn process_status() -> String {
+    "Rust Engine is Active".to_string()
+}
